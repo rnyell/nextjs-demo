016dfa423ed0f96309202f1098e5468e1cfba7f7
rnyell <cisamirreza@gmail.com>
2024-01-09 01:31:20 +0330

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h2 className={`bg-orange-400 h-[15vh]`}>Customer Page</h2>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  // console.log('yo yo')
+  return <h2>Dashboard Page</h2>
+}
